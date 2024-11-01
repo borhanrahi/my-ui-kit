024db016c8530c8fe5ede81e2caa8026322e8d29
borhanrahi <borhanrahi111@gmail.com>
2024-11-02 00:02:31 +0600

    fujk

--- app/live-components/[componentName]/page.tsx
+++ app/live-components/[componentName]/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from 'next/navigation';
 import docs from '@/configs/docs.json';
 import dynamic from 'next/dynamic';
 
-export async function generateStaticParams(): Promise<{ componentName: string }[]> {
+export async function generateStaticParams() {
   const paths = docs.dataArray.flatMap((category) =>
     category.componentArray.map((component) => ({
       componentName: component.componentName,
@@ -20,7 +20,7 @@ export default async function Page({
   searchParams?: { [key: string]: string | undefined };
 }) {
   const { componentName } = params;
-  console.log(componentName);
+  // console.log(componentName);
 
   // Find the component data based on componentName
   const component = docs.dataArray.reduce((found, category) => {
@@ -31,13 +31,11 @@ export default async function Page({
     );
   }, null as any);
 
-  console.log(componentName);
+  // console.log(componentName);
 
   if (!component) {
     notFound();
   }
-
-  const isFramerScrolling = componentName === 'framerhorizontalscroll';
 
   const ComponentPreview = component?.filesrc
     ? dynamic(() => import(`../../../registry/${component.filesrc}`), {
@@ -46,11 +44,7 @@ export default async function Page({
     : null;
 
   return (
-    <section
-      className={`${
-        isFramerScrolling ? '' : 'flex justify-center items-center '
-      } min-h-screen rounded-md dark:bg-[#000000] bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:20px_20px]`}
-    >
+    <section className="flex justify-center items-center min-h-screen rounded-md dark:bg-[#000000] bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:20px_20px]">
       <div className="px-4 w-full">
         {ComponentPreview ? (
           <Suspense fallback={<div>Loading preview...</div>}>
